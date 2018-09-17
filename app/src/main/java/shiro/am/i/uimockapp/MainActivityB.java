5da5d1ae77a3877336366b352aba17920547484d
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2018-09-17 19:03:31 +0800

    checkpoint

--- app/src/main/java/shiro/am/i/uimockapp/MainActivityB.java
+++ app/src/main/java/shiro/am/i/uimockapp/MainActivityB.java
@@ -3,27 +3,27 @@ package shiro.am.i.uimockapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivityB extends AppCompatActivity{
+public class MainActivityB extends AppCompatActivity {
 
     int layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_1b);
+        setContentView(R.layout.b_layout_1);
         layout = 1;
     }
 
     @Override
     public void onBackPressed() {
         if (layout == 1) {
-            setContentView(R.layout.layout_2b);
+            setContentView(R.layout.b_layout_2);
             layout = 2;
         } else if (layout == 2) {
-            setContentView(R.layout.layout_3b);
+            setContentView(R.layout.b_layout_3);
             layout = 3;
         } else if (layout == 3) {
-            setContentView(R.layout.layout_1b);
+            setContentView(R.layout.b_layout_1);
             layout = 1;
         }
     }
--- app/src/main/java/shiro/am/i/uimockapp/MainActivityC.java
+++ app/src/main/java/shiro/am/i/uimockapp/MainActivityC.java
@@ -3,27 +3,27 @@ package shiro.am.i.uimockapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivityA extends AppCompatActivity {
+public class MainActivityC extends AppCompatActivity {
 
     int layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.a_layout_1);
+        setContentView(R.layout.c_layout_1);
         layout = 1;
     }
 
     @Override
     public void onBackPressed() {
         if (layout == 1) {
-            setContentView(R.layout.a_layout_2);
+            setContentView(R.layout.c_layout_2);
             layout = 2;
         } else if (layout == 2) {
-            setContentView(R.layout.a_layout_3);
+            setContentView(R.layout.c_layout_3);
             layout = 3;
         } else if (layout == 3) {
-            setContentView(R.layout.a_layout_1);
+            setContentView(R.layout.c_layout_1);
             layout = 1;
         }
     }
